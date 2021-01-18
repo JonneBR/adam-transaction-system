3abcdc6a7c1859cfa4bc30fcf0d4a5604d1a019f
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-18 14:20:37 -0300

    hero section css until top line concluded

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'Button';
+import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './HeroSection.css';
 
@@ -18,41 +18,42 @@ function HeroSection({
   return (
     <>
       <div
-        className={lightBg ? 'home__hero-secton' : 'home__hero-section darkBg'}
-      ></div>
-      <div className="container">
-        <div
-          className="row home__hero-row"
-          style={{
-            display: 'flex',
-            flexDirection: imgStart === 'start' ? 'row-reverse' : 'row',
-          }}
-        >
-          <div className="col">
-            <div className="home__hero-text-wrapper">
-              <div className="top-line">{topLine}</div>
-              <h1 className={lightText ? 'heading' : 'heading dark'}>
-                {headline}
-              </h1>
-              <p
-                className={
-                  lightTextDesc
-                    ? 'home__hero-subtitle'
-                    : 'home__hero-subtitle dark'
-                }
-              >
-                {description}
-              </p>
-              <Link to="/sign-up">
-                <Button buttonSize="btn--wide" buttonColor="blue">
-                  {buttonLabel}
-                </Button>
-              </Link>
+        className={lightBg ? 'home__hero-section' : 'home__hero-section darkBg'}
+      >
+        <div className="container">
+          <div
+            className="row home__hero-row"
+            style={{
+              display: 'flex',
+              flexDirection: imgStart === 'start' ? 'row-reverse' : 'row',
+            }}
+          >
+            <div className="col">
+              <div className="home__hero-text-wrapper">
+                <div className="top-line">{topLine}</div>
+                <h1 className={lightText ? 'heading' : 'heading dark'}>
+                  {headline}
+                </h1>
+                <p
+                  className={
+                    lightTextDesc
+                      ? 'home__hero-subtitle'
+                      : 'home__hero-subtitle dark'
+                  }
+                >
+                  {description}
+                </p>
+                <Link to="/sign-up">
+                  <Button buttonSize="btn--wide" buttonColor="blue">
+                    {buttonLabel}
+                  </Button>
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className="col">
-            <div className="home__hero-img-wrapper">
-              <img src={img} alt={alt} className="home__hero-img" />
+            <div className="col">
+              <div className="home__hero-img-wrapper">
+                <img src={img} alt={alt} className="home__hero-img" />
+              </div>
             </div>
           </div>
         </div>
