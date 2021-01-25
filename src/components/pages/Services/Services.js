ab1f5ade67b78f62a4b230d5bca7ab3b7dba5aab
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-25 13:13:13 -0300

    comments deleted

--- src/components/pages/Services/Services.js
+++ src/components/pages/Services/Services.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import HeroSection from '../../HeroSection';
 import Pricing from '../../Pricing';
-import { homeObjOne } from './Data';
 
 function Home() {
   return (
     <>
       <Pricing />
-      {/* <HeroSection {...homeObjOne} /> */}
     </>
   );
 }
