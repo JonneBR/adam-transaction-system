f255832a648529b07bc77520b4416357e3c5ab84
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-25 06:02:40 -0300

    pricing component

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -5,70 +5,69 @@ import { BsXDiamondFill } from 'react-icons/bs';
 import { GiCrystalize } from 'react-icons/gi';
 import { IconContext } from 'react-icons/lib';
 import { Link } from 'react-router-dom';
+import './Pricing.css';
 
 function Pricing() {
   return (
     <IconContext.Provider value={{ color: '#fff', size: 64 }}>
-      <div>
-        <div className="pricing__section">
-          <div className="pricing__wrapper">
-            <h1 className="pricing__heading">Pricing</h1>
-            <div className="pricing__container">
-              <Link to="/sign-up" className="pricing__container-card">
-                <div className="pricing__continer-cardInfo">
-                  <div className="icon">
-                    <FaFire />
-                    <h3>Starter</h3>
-                    <h4>$8.99</h4>
-                    <p>per month</p>
-                    <ul className="pricing__container-features">
-                      <li>100 Transaction</li>
-                      <li>2% Cash Back</li>
-                      <li>$10,000 Limit</li>
-                      <Button buttonSize="btn--wide" buttonColor="primary">
-                        Choose Plan
-                      </Button>
-                    </ul>
-                  </div>
+      <div className="pricing__section">
+        <div className="pricing__wrapper">
+          <h1 className="pricing__heading">Pricing</h1>
+          <div className="pricing__container">
+            <Link to="/sign-up" className="pricing__container-card">
+              <div className="pricing__container-cardInfo">
+                <div className="icon">
+                  <FaFire />
                 </div>
-              </Link>
-              <Link to="/sign-up" className="pricing__container-card">
-                <div className="pricing__continer-cardInfo">
-                  <div className="icon">
-                    <BsXDiamondFill />
-                    <h3>Gold</h3>
-                    <h4>$29.99</h4>
-                    <p>per month</p>
-                    <ul className="pricing__container-features">
-                      <li>1000 Transaction</li>
-                      <li>3.5% Cash Back</li>
-                      <li>$100,000 Limit</li>
-                      <Button buttonSize="btn--wide" buttonColor="primary">
-                        Choose Plan
-                      </Button>
-                    </ul>
-                  </div>
+                <h3>Starter</h3>
+                <h4>$8.99</h4>
+                <p>per month</p>
+                <ul className="pricing__container-features">
+                  <li>100 Transaction</li>
+                  <li>2% Cash Back</li>
+                  <li>$10,000 Limit</li>
+                  <Button buttonSize="btn--wide" buttonColor="primary">
+                    Choose Plan
+                  </Button>
+                </ul>
+              </div>
+            </Link>
+            <Link to="/sign-up" className="pricing__container-card">
+              <div className="pricing__container-cardInfo">
+                <div className="icon">
+                  <BsXDiamondFill />
                 </div>
-              </Link>
-              <Link to="/sign-up" className="pricing__container-card">
-                <div className="pricing__continer-cardInfo">
-                  <div className="icon">
-                    <GiCrystalize />
-                    <h3>Diamong</h3>
-                    <h4>$99.99</h4>
-                    <p>per month</p>
-                    <ul className="pricing__container-features">
-                      <li>Unlimited Transaction</li>
-                      <li>5% Cash Back</li>
-                      <li>Unlimited Spending</li>
-                      <Button buttonSize="btn--wide" buttonColor="primary">
-                        Choose Plan
-                      </Button>
-                    </ul>
-                  </div>
+                <h3>Gold</h3>
+                <h4>$29.99</h4>
+                <p>per month</p>
+                <ul className="pricing__container-features">
+                  <li>1000 Transaction</li>
+                  <li>3.5% Cash Back</li>
+                  <li>$100,000 Limit</li>
+                  <Button buttonSize="btn--wide" buttonColor="blue">
+                    Choose Plan
+                  </Button>
+                </ul>
+              </div>
+            </Link>
+            <Link to="/sign-up" className="pricing__container-card">
+              <div className="pricing__container-cardInfo">
+                <div className="icon">
+                  <GiCrystalize />
                 </div>
-              </Link>
-            </div>
+                <h3>Diamong</h3>
+                <h4>$99.99</h4>
+                <p>per month</p>
+                <ul className="pricing__container-features">
+                  <li>Unlimited Transaction</li>
+                  <li>5% Cash Back</li>
+                  <li>Unlimited Spending</li>
+                  <Button buttonSize="btn--wide" buttonColor="primary">
+                    Choose Plan
+                  </Button>
+                </ul>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
